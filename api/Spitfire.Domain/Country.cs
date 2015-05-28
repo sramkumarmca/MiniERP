f916798dcc7ece45ab7e67c3e330ef46613205a7
LaurentzT <laurentz.tonniau@firmware.be>
2015-05-28 12:06:11 +0200

    Add Cirty Domain

---
+++ api/Spitfire.Domain/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spitfire.Domain
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Country(string name)
+        {
+            Name = name;
+        }
+    }
+}
